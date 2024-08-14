2f752a5c8ebd30c991bdf432fe032785edaf3de4
unknown <harrison.m.sand@gmail.com>
2024-08-14 13:20:58 -0500

    Updates with exercises complete for chapter 12

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,12 +1,41 @@
 # Assign the variables for exercise 1 here:
 
+# I'm using type annotations to practice. Learned it online. It just is a way of labeling the type in a clear to read way.
+engine_indicator_light: str = 'red blinking'
+space_suits_on: bool = True
+shuttle_cabin_ready: bool = True
+crew_status: bool = space_suits_on and shuttle_cabin_ready
+computer_status_code: int = 200
+shuttle_speed: int = 15000
 
 
-# BEFORE running the code, predict what will be printed to the console by the following statements:
+# Q2 BEFORE running the code, predict what will be printed to the console by the following statements: It will print "enginges are off"
 
-if engine_indicator_light == "green": 
-  print("engines have started")
-elif engine_indicator_light == "green blinking": 
-  print("engines are preparing to start")
+# if engine_indicator_light == "green": 
+#   print("engines have started")
+# elif engine_indicator_light == "green blinking": 
+#   print("engines are preparing to start")
+# else:
+#   print("engines are off")
+
+# Q3.1
+# if crew_status:
+#     print("Crew Ready")
+# else:
+#     print("Crew not Ready")
+
+# Q3.2
+# if computer_status_code == 200:
+#     print("Please Stand by. Computer is rebooting.")
+# elif computer_status_code == 400:
+#     print("Success! Computer online.")
+# else:
+#     print("ALERT: Computer offline!")
+
+# Q3.3
+if shuttle_speed > 17_500:
+    print("ALERT: Escape velocity reached!")
+elif shuttle_speed < 8_000:
+    print("Alert: Cannot maintain orbit!")
 else:
-  print("engines are off")
+    print("Stable speed.")
--- how-to-write-code/Test File.py
+++ how-to-write-code/Test File.py
@@ -1,5 +1,20 @@
-num1 = input("Enter a number?")
-num2 = input("Enter another number?")
-num1= int(num1)
-num2 = int(num2)
-print(num1 + num2);
+# name = "f-string"
+# print(f"Hello, {name}! This is on one line.")
+# print("Hello,", end=" ")
+# print(" How ", end=" ")
+# print(" are you? ")
+# print("Hello, {name}! This is onone line.")
+# print("try this, 'this is in quotes'") #best practice
+# print("try this, \"this is in quotes\"")
+# print("""try this, 'this is in quotes" """)
+
+# This is a long comment, 
+# will it seperate into a new line?
+
+# rabbit = None
+# print(type(rabbit))
+
+
+
+
+
