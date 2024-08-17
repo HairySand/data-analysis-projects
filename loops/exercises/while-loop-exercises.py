e7876fa258757dee64175a6d4485fe3e48dc56a4
unknown <harrison.m.sand@gmail.com>
2024-08-17 09:26:14 -0500

    Fixed error in indent and error in negative fuel result.

--- loops/exercises/while-loop-exercises.py
+++ loops/exercises/while-loop-exercises.py
@@ -1,22 +1,23 @@
 # Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
 start_fuel = 1000
-num_astro = 3
 top_alt = 2000
 altitude = 0
-
+num_astro  = 0
+testastro = 0
+testnumfuel = 0
 
 # Exercise #1: Construct while loops to do the following:
   # a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. 
 start_fuel =int(input("What is starting fuel level?  "))
-testnumfuel = 0
+
 
 while testnumfuel == 0:
   if start_fuel <5000:
-    print("Fuel level too low! Fuel level must be above 5,000 gallons.")
+    print("Fuel level too low! Fuel level must be above 5,000 kg.")
     start_fuel =int(input("What is starting fuel level?  "))
   elif start_fuel > 30_000:
-    print("Fuel level to high! Fuel must be under 30,000 gallons.")
+    print("Fuel level too high! Fuel must be under 30,000 kg.")
     start_fuel =int(input("What is starting fuel level?  "))
   else:
     testnumfuel += 1
@@ -25,10 +26,9 @@ while testnumfuel == 0:
 
 # b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
-  num_astro  = int(input("How many astronauts are aboard?  "))
-  testastro = 0
+num_astro = int(input("How many astronauts are aboard?  "))
 
-  while testastro == 0:
+while testastro == 0:
     if num_astro > 7:
       print("Too many Astronauts! Remove someone from the mission. Max Astronauts = 7.")
       num_astro = int(input("How many astronauts are aboard? (Remember 7 or less!)  "))
@@ -50,7 +50,7 @@ while testnumfuel == 0:
 # Exercise #2: Print the result with the phrase, The shuttle gained an altitude of ___ km and has ___ kg of fuel left. Fill in the blanks with the altitude and fuel level values.
 # If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”
 
-while start_fuel >0:
+while start_fuel >99 * num_astro:
   altitude += 50
   start_fuel -= 100 * num_astro
 
