87bc7c355dc86602bdf34e0ff3c1599169a7f744
unknown <harrison.m.sand@gmail.com>
2024-09-10 22:07:27 -0500

    Studio added

--- Classroom Test Files/Class 12 Inclass.py
+++ Classroom Test Files/Class 12 Inclass.py
@@ -1,62 +1,63 @@
-# def repeater(str):
-#    repeated = str + str
-#    print(repeated)
+# # def repeater(str):
+# #    repeated = str + str
+# #    print(repeated)
 
-# repeater('Bob')
+# # repeater('Bob')
 
-def reverse_characters(chars):
-        rev_str_list = chars[::-1]
-        chars = "".join(rev_str_list)
-        return chars
+# def reverse_characters(chars):
+#         rev_str_list = chars[::-1]
+#         chars = "".join(rev_str_list)
+#         return chars
 
 
-def reverse_characters(chars):
-    if type(chars) == str:
-        rev_str_list = chars[::-1]
-        chars = "".join(rev_str_list)
-        return chars
-    elif type(chars) == int:
-        chars = str(chars)
-        rev_str_list = chars[::-1]
-        chars = "".join(rev_str_list)
-        chars = int(chars)
-        return chars
-    elif type(chars) == float:
-        chars = str(chars)
-        rev_str_list = chars[::-1]
-        chars = "".join(rev_str_list)
-        chars = float(chars)
-        return chars
-    elif type(chars) == bool:
-        chars =  not chars
-        bool(chars)
-        return chars
+# def reverse_characters(chars):
+#     if type(chars) == str:
+#         rev_str_list = chars[::-1]
+#         chars = "".join(rev_str_list)
+#         return chars
+#     elif type(chars) == int:
+#         chars = str(chars)
+#         rev_str_list = chars[::-1]
+#         chars = "".join(rev_str_list)
+#         chars = int(chars)
+#         return chars
+#     elif type(chars) == float:
+#         chars = str(chars)
+#         rev_str_list = chars[::-1]
+#         chars = "".join(rev_str_list)
+#         chars = float(chars)
+#         return chars
+#     elif type(chars) == bool:
+#         chars =  not chars
+#         bool(chars)
+#         return chars
   
 
 
-print(reverse_characters(True))
+# print(reverse_characters(True))
 
-def double_rev_list(rlist):
-    rev_list = rlist[::-1]
-    for i in range(len(rev_list)):
-        rev_list[i] = reverse_characters(rev_list[i])
-    return rev_list
-
-
-print(double_rev_list(["Hello", "Brief", 542]))
+# def double_rev_list(rlist):
+#     rev_list = rlist[::-1]
+#     for i in range(len(rev_list)):
+#         rev_list[i] = reverse_characters(rev_list[i])
+#     return rev_list
 
 
+# print(double_rev_list(["Hello", "Brief", 542]))
 
-# def add_num_together(num = 100):
-#     total = 0
-#     while num < 100:
-#         total += num
-#         num += 1
 
-#     return total , num
+
+# # def add_num_together(num = 100):
+# #     total = 0
+# #     while num < 100:
+# #         total += num
+# #         num += 1
+
+# #     return total , num
     
-# print(add_num_together(55))
+# # print(add_num_together(55))
 
+import pandas as pd
 
 
 
